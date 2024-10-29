270be6ba9f6be5c8e626a3d03fb85c8545e3bdc8
braden <leachbr787@northwested.org>
2024-10-29 13:05:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+#Braden Leach
+#OCT 29 2024
+#Lists Evidence
+
+
+#empty lists
+name = []
+name2 = []
+school = []
+school2 = []
+course = []
+course2 = []
+score = [50,99,87,67]
+score2 = [55,99,97,87]
+#add
+addname = name.append('Jack')
+addname2 = name2.append('Sal')
+addschool = school.append('West High')
+addschool2 = school2.append('East High')
+addcourse = course.append('history')
+addcourse2 = course2.append('math')
+addscore = score.append(50,99,87,67)
+addscore2 = score2.append(55,99,97,87)
+#Math for score
+
+
+
+
+#Display
+print(f'''
+Semester 1 Grade Report
+-------------
+Student: {addname}
+School: {addschool}
+Course:{addcourse}
+Average Score: 
+      ''')
+
+
+print(f'''
+Student: {addname2}
+School: {addschool2}
+Course:{addcourse2}
+Average Score: 
+      ''')
